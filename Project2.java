aa498aa52951303d515d42a5b5fe687d4931377c
Dtakuto <truetakuto2023@gmail.com>
2025-04-03 21:12:46 -0500

    Find the closest stores

--- Project2.java
+++ Project2.java
@@ -1,12 +1,14 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
-<<<<<<< Updated upstream
+
 public class Project2{
 
     public static List<Store> readFile(String filePath) throws Exception {
         ArrayList<Store> storeList = new ArrayList<Store>();
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        reader.readLine(); // Skip the header line if there is one
 
         String line; 
 
@@ -19,6 +21,7 @@ public class Project2{
             String zip = storeData[4];
             double latitude = Double.parseDouble(storeData[5]);
             double longitude = Double.parseDouble(storeData[6]);
+            storeList.add(new Store(storeID, address, city, state, zip, latitude, longitude));
 
         }
         reader.close();
@@ -26,9 +29,10 @@ public class Project2{
 
     }
 
-    public static List<Store> readQuery(String filePath) throws Exception {
-        ArrayList<Store> queryList = new ArrayList<Store>();
+    public static List<Query> readQuery(String filePath) throws Exception {
+        ArrayList<Query> queryList = new ArrayList<Query>();
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        reader.readLine(); // Skip the header line if there is one
 
         String line; 
 
@@ -37,6 +41,7 @@ public class Project2{
             double queryLat = Double.parseDouble(queryData[0]);
             double queryLong = Double.parseDouble(queryData[1]);
             int storesDesired = Integer.parseInt(queryData[2]);
+            queryList.add(new Query(queryLat, queryLong, storesDesired));
 
         }
         reader.close();
@@ -51,24 +56,16 @@ public class Project2{
 
        System.out.println("Read in Queries file...");
 
-       List <Store> queryList = readQuery("Queries.csv");
+       List <Query> queryList = readQuery("Queries.csv");
+
+       for (Query q : queryList){
+            ArrayList<Store> nearbyStores = new ArrayList<Store>();
+            for (Store s : storeList) {
+                Store.computeDistance(q.theLat, q.queryLong); // compute distance for each store
+            }
+       }
     }
 
 }
 
     
-   
-        
-
-
-
-=======
-public class Project2 {
-    
-
-}
-
-public static void main(String[] args) throws Exception {
-
-}
->>>>>>> Stashed changes
---
+++ Query.java
@@ -0,0 +1,13 @@
+public class Query {
+    public double latitude;
+    public double longitude;
+    public int storesDesired;
+
+    public Query(double theLat, double theLong, int theStoresDesired) {
+        latitude = theLat;
+        longitude = theLong;
+        storesDesired = theStoresDesired;
+    }
+
+    
+}
